f4f94330cb22ca9638064bd7284ee732cbc69f51
Dvontre Coleman <dvontrec@gmail.com>
2018-08-06 20:02:50 -0400

    Setup spotify authentication, Invalid auth code ATM

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 const User = require('./models/User');
 const port = process.env.PORT;
 const app = express();
+
+const mongoURL = process.env.DATABASEURL || 'mongodb://localhost/hello-spotify';
+
+mongoose.connect(mongoURL);
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -13,12 +19,14 @@ const app = express();
 //   the user by ID when deserializing. However, since this example does not
 //   have a database of user records, the complete spotify profile is serialized
 //   and deserialized.
-passport.serializeUser(function(user, done) {
-	done(null, user);
+passport.serializeUser((user, done) => {
+	done(null, user.id);
 });
 
-passport.deserializeUser(function(obj, done) {
-	done(null, obj);
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
 });
 
 passport.use(
@@ -28,30 +36,39 @@ passport.use(
 			clientSecret: process.env.CLIENT_SECRET,
 			callbackURL: 'http://localhost:3005/auth/spotify/callback'
 		},
-		function(accessToken, refreshToken, expires_in, profile, next) {
+		async (accessToken, refreshToken, expires_in, profile, done) => {
 			console.log(profile);
-			User.findOrCreate(
-				{ spotifyId: profile.id, name: profile.displayName },
-				(err, user) => {
-					console.log('CREATING user: ', user);
-					return next(err, user);
-				}
-			);
+			const existingUser = await User.findOne({ spotifyId: profile.id });
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({ spotifyId: profile.id }).save;
+			done(null, user);
 		}
 	)
 );
 
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, //saves the cookie for 30 days 24 hours in a day 60 minutes in an hour 60 seconds in a minute 1000 milliseconds in a second
+		// encrypts the cookie
+		keys: [process.env.COOKIE_KEY_1]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', require('./routes/index'));
-app.get('/auth/spotify', passport.authenticate('spotify'), function(req, res) {
+app.get('/auth/spotify', passport.authenticate('spotify', {}), (req, res) => {
 	// The request will be redirected to spotify for authentication, so this
 	// function will not be called.
 });
 app.get(
 	'/auth/spotify/callback',
-	passport.authenticate('spotify', { failureRedirect: '/login' }),
-	function(req, res) {
+	passport.authenticate('spotify', { failureRedirect: '/wrong' }),
+	(req, res) => {
 		// Successful authentication, redirect home.
-		res.redirect('/spot');
+		res.redirect('/');
 	}
 );
 
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,8 @@ router.get('/spot', (req, res) => {
 	res.send('BINGO!!!!');
 });
 
+router.get('/wrong', (req, res) => {
+	res.send('Cannot login');
+});
+
 module.exports = router;
